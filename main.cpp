a0709fd0aff31f6d79cde40e30b3895e68f6529f
gol <osno2806@gmail.com>
2025-12-14 21:08:26 +0500

    каркас

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+void func1()
+{
 
+}
+void func2()
+{
 
+}
+void func3()
+{
+
+}
 int main()
 {
+    func1();
+    func2();
+    func3();
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
